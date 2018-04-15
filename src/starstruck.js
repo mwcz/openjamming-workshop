04d0fe1a79c1aaf7fc7cf1d0789650d5d4a38489
Michael Clayton <mclayton@redhat.com>
2018-04-14 21:11:50 -0400

    add collision handler between player and enemy

--- src/starstruck.js
+++ src/starstruck.js
@@ -88,9 +88,11 @@ function create() {
     player.animations.add('turn', [4], 20, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    enemy = game.add.sprite(32, 32, 'droid');
-    enemy.anchor.setTo(0.5, 0.5);
+    enemy = game.add.sprite(200, 32, 'droid');
     game.physics.enable(enemy, Phaser.Physics.ARCADE);
+    enemy.body.collideWorldBounds = true;
+    // enemy.body.setSize(20, 32, 5, 16);
+    enemy.anchor.setTo(0.5, 0.5);
 
     enemy.animations.add('move', [0, 1, 2, 3], 10, true);
 
@@ -104,6 +106,7 @@ function create() {
 function update() {
 
     game.physics.arcade.collide([player, enemy], layer);
+    game.physics.arcade.collide(player, enemy, handlePlayerHitEnemy);
 
     player.body.velocity.x = 0;
 
@@ -181,4 +184,13 @@ function render () {
     // game.debug.bodyInfo(enemy, 16, 24);
 
 
+}
+
+function handlePlayerHitEnemy(player, enemy) {
+    if (player.body.touching.down) {
+        enemy.kill();
+    }
+    else {
+        player.kill()
+    }
 }
